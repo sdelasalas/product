4ce76b83a03520fa9e19c38de260b6f8128b359b
sdelasalas <stacy.delasalas@gmail.com>
2020-07-03 14:36:51 -0400

    added remove button from compare

--- src/components/products.js
+++ src/components/products.js
@@ -19,42 +19,34 @@ class Product extends Component {
             {'name': 'Beach Trip 4', 'img': '4.jpg', 'id': 4, 'price': '$ 1,000', 'package': 'none'},
             {'name': 'Beach Trip 5', 'img': '5.jpg', 'id': 5, 'price': '$ 2,500', 'package': 'all inclusive'},
         ],
-        compare: {
-            //the arr variable stores the id of the selected components
-            arr: []
-        }
+        compare: []
     }
     this.handleClick = this.handleClick.bind(this);
+    this.removeProduct = this.removeProduct.bind(this);
 }
 
 //e is for event - here that is 'click'
 //this variable has some information like: name, id, etc. of the component which fired it
 handleClick(e){
-    let arrnew = this.state.compare.arr;
-    let temp = arrnew.indexOf(e.target.id)
+    const { products, compare } = this.state;
+    const id = parseInt(e.target.id);
     
-    if (temp !== -1){
-        //if present then remove it
-        arrnew.splice(temp, 1)
+    const selectedVacation = products.find(x => x.id === parseInt(id));
+    if(!compare.find(c => c === selectedVacation)){
+    this.setState({compare: [...compare, selectedVacation]});
     }
-    else {
-        arrnew.push(e.target.id)
-    }
-    //setState function helps in changing the state.
-    this.setState({ compare: {arr: arrnew} });
+}
+// TODO: Refactor to remove product from compare array based on id 
+ 
+removeProduct(e) {
+    const id = parseInt(e.target.id);
+    const remove = this.state.compare.filter((x) => x.id !== id);
+    this.setState({compare: remove});
+
 }
 
 //this function renders a component
 render () {
-    let temparr = this.state.compare.arr
-    let temp = []
-    let Compare;
-    for (var i=0; i<temparr.length; i++){
-        let x = this.state.products.find(prod => prod.id === temparr[i]);
-        temp.push(x)
-    }
-    
-    //whatever is returned is what is rendered
     return(
         <>
         <Container fluid>
@@ -68,7 +60,7 @@ render () {
                 <Card  body outline engine="primary">
                     <img variant="top"  src={require("../assets/images/" + product.img)} alt={product.name} />
                     <CardTitle>{product.name}</CardTitle>
-                    <Button outline color="info"type="button" id={product.id} onClick = {this.handleClick}>{(this.state.compare.arr.indexOf(product.id) < 0) ? "Compare" : "Remove"}</Button>
+                    <Button outline color="info"type="button" id={product.id} onClick = {this.handleClick}>Compare</Button>
                     </Card>
                 </Col>
         ))}
@@ -79,13 +71,15 @@ render () {
                 <th width="33.33%">Name</th>
                 <th width="33.33%">Price</th>
                 <th width="33.33%">Package</th>
+                <th>Action</th>
             </thead>
-                {temp.map((product)=> (
+                {this.state.compare.length > 0 && this.state.compare.map((product)=> (
                     <tbody>
                         <tr>
                             <td width="33.33%">{product.name}</td>
                             <td width="33.33%">{product.price}</td>
                             <td width="33.33%">{product.package}</td>
+                            <td><button id={product.id} onClick={this.removeProduct}>Remove</button></td>
                         </tr>
                     </tbody>
                 ))}
